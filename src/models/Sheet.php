e6ec7a778aaddaf1eae98d82e31488b77072dead
misha <misha.behey@gmail.com>
2017-04-25 00:35:34 -0700

    keep working on the sheet model

--- src/models/Sheet.php
+++ src/models/Sheet.php
@@ -12,6 +12,7 @@ class Sheet extends Model
     public $data;
     public $hash_codes;
     public $sheet_name;
+    public $id_valid;
 
     public function __construct()
     {
@@ -32,5 +33,43 @@ class Sheet extends Model
         $this->data = $data;
         //in here we need something to instantiate a new sheet
 
+        //connection to the database
+        $mysqli = parent::connectTO("db");
+
+        //handle connection failure
+        if ($mysqli->connect_errno)
+        {
+            print ("failed to connect");
+        }
+
+        //we need to handle the IDs in here somehow
+
+    }
+
+
+    //method to get data about existing sheet
+    public function retrieveSheet ()
+    {
+        //connect to database
+        $mysqli = parent::connectTO("db"); //connect to db
+
+        //handle error
+        if ($mysqli->connect_errno)
+        {
+            print ("connection error");
+        }
+
+        // here we need a method to get an ID from the database
+
+        if ($this->id < 0)
+        {
+            $this->id_valid = false;
+            print ("can't be less than 0");
+        }
+
+        else
+        {
+            $this->id_valid = true; // id is aight
+        }
     }
 }
