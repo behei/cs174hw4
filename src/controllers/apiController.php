4d4735cd5803d7d8010151319ec5bc5dab30df0d
rjl1361 <rjl136@live.com>
2017-04-28 15:17:07 -0700

    Add files via upload

--- src/controllers/apiController.php
+++ src/controllers/apiController.php
@@ -5,13 +5,3 @@
  * Date: 4/24/17
  * Time: 12:11 AM
  */
-
-namespace HMRTeam\hw4\controllers;
-
-use \HMRTeam\hw4\views as views;
-use \HMRTeam\hw4\models as models;
-
-class apiController
-{
-
-}
