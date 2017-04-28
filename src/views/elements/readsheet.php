f6827fed878174959c9199a22ad281230a556553
misha <misha.behey@gmail.com>
2017-04-28 05:50:58 -0700

    added a read sheet view

---
+++ src/views/elements/readsheet.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mishabehey
+ * Date: 4/20/17
+ * Time: 2:19 AM
+ */
+class readsheet
+{
+    public function render($sheet_id, $sheet_data, $hash_code1, $hash_code2, $hash_code3)
+    {
+        $layout = new Layouts();
+        $layout->header();
+        echo '
+            <h1><a href="index.php?c=Home&m=index">Web Sheets</a>' . $sheet_id . '</h1>
+            <b>File URL:</b>
+           <input type="text" value="'.BASE_URL."index.php?c=Home&m=view&arg1=".$hash_code3.'">
+        ';
+
+        echo ' <script src="./src/scripts/spreadsheet.js"></script>
+                  <script type="text/javascript"> 
+                  spreadsheet =  new Spreadsheet("' . $sheet_id . '",' . $sheet_data . ',{"mode":"read"});
+                  spreadsheet.draw();
+                   
+                    </script>';
+       $layout->footer();
+    }
+
+}
