b7b2d03222de665951cb5cde46fbecce1eb5c0ca
rjl1361 <rjl136@live.com>
2017-04-28 18:03:50 -0700

    Add files via upload

--- src/controllers/apiController.php
+++ src/controllers/apiController.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mishabehey
- * Date: 4/24/17
- * Time: 12:11 AM
- */
+
+class api {
+
+    public function update() {
+        // log the update
+        $logger = new Helper();
+        $logger->logger("Visited Updating via Ajax Page");
+
+        // post the update
+        $name = trim($_POST['sheetname']);
+        $data = trim($_POST['sheetdata']);
+        $model = new sheet_code();
+        $model->updatesheet($sheet_data, $sheet_name);
+    }
+
+}
